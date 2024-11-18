e5de38bf2f4569b12827d470fea343d1f9a94412
himashaban <himashaban75@gmail.com>
2024-11-18 10:38:45 +0200

    ensure mobile view is working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./index.html",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
 
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "640px", // Small screens (e.g., mobile)
+        md: "768px", // Medium screens (e.g., tablets)
+        lg: "1024px", // Large screens (e.g., laptops/desktops)
+        xl: "1280px", // Extra large screens (e.g., large desktops)
+        "2xl": "1536px", // Very large screens
+      },
+      colors: {
+        customGreen: "#00ff00", // Add your custom colors
+        customBlue: "#0000ff",
+      },
+      spacing: {
+        128: "32rem", // Custom spacing values (e.g., 128 can be used as spacing or width)
+      },
+    },
   },
+
   plugins: [],
-  
 };
